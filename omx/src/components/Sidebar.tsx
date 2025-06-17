0c8c29f99be7b16423b961658e2878d1582473b1
ElectricHopelite <franculetuoctavian@gmail.com>
2025-06-17 22:13:25 +0300

    ziua 9, eroare tailwind

--- omx/src/components/Sidebar.tsx
+++ omx/src/components/Sidebar.tsx
@@ -11,14 +11,14 @@ function Sidebar() {
   const showSidebar = () => setSidebar(!sidebar);
   return (
     <nav className={sidebar ? "sidebar active" : "sidebar"}>
-      <button className="hamburger" type="button" onClick={showSidebar}>
+      <button className="m-4 " type="button" onClick={showSidebar}>
         <div><MenuIcon/></div>
       </button>
-      <ul className="list-none" onClick={showSidebar}>
-        <li className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 dark:bg-gray-800 dark:text-gray-200"><Link to="/"><HomeIcon/> Home</Link></li>
-        <li><Link to="/event"><SportsScoreIcon/>Events</Link></li>
-        <li><Link to="/profile"><AccountCircleIcon/>Profile</Link></li>
-        <li><Link to="/admin"><AdminPanelSettingsIcon/>Admin</Link></li>
+      <ul className="list-none w-3xs" onClick={showSidebar}>
+        <li className="inline-block rounded-sm m-1 bg-red-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"><Link to="/"><HomeIcon/> Home</Link></li>
+        <li className="inline-block rounded-sm m-1 bg-red-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"><Link to="/event"><SportsScoreIcon/>Events</Link></li>
+        <li className="inline-block rounded-sm m-1 bg-red-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"><Link to="/profile"><AccountCircleIcon/>Profile</Link></li>
+        <li className="inline-block rounded-sm m-1 bg-red-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"><Link to="/admin"><AdminPanelSettingsIcon/>Admin</Link></li>
 
       </ul>
     </nav>
